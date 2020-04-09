a9c57691743a8c42488f30fee5bdc2aed70054a7
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-09 20:18:12 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board,index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else board[index] != " "
+    false
+  end
+end
