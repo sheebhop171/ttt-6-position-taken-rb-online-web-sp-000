4b166dd8f3aab8597bd0dc9fd4ab0f61d93177b5
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-09 20:14:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] != "  "
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+
+
+
+
+
+# returns false if board has an empty string "  " in the desired index
+# returns flase if board has an empty string " "
